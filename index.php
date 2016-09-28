acee32dfebd99348877fc0629281cb1a34f64806
Marc Barbeau <hello@marcbarbeau.com>
2016-09-28 16:54:12 -0600

    Fix typo

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ If you are familiar with Big O notation, show your analysis.
 */
 
 $nav = [
-        [1, 'Item1', 0,],
-        [2 ,'Item2', 0 ],
+        [1, 'Item1', 0],
+        [2 ,'Item2', 0],
         [3, 'Item3', 0],
         [4, 'Item10', 1],
         [5, 'Item11', 1],
@@ -42,4 +42,5 @@ $nav = [
       ];
 
 var_dump($nav);
+
 ?>
