75062a7ee26092c745379feff06ad7e9b6860223
Marc Barbeau <hello@marcbarbeau.com>
2016-09-28 16:49:30 -0600

    Hardcode data

--- index.php
+++ index.php
@@ -5,48 +5,6 @@ Please provide your own custom working solution to the following problem in conc
 
 We want to be able to output a multi-level nested list of items in HTML. A simple SQL table would have the following data elements: itemID – int, item – varchar, subItemOfID – int, where subItemOfID contains the itemID of the parent item. A sample of the data would look something like this:
 
-
-itemID
-Item
-subItemOfID
-1
-Item1
-0
-2
-Item2
-0
-3
-Item3
-0
-4
-Item10
-1
-5
-Item11
-1
-6
-Item12
-1
-7
-Item100
-4
-8
-Item101
-4
-9
-Item102
-4
-10
-Item30
-3
-11
-Item31
-3
-12
-Item32
-3
-
-
 The HTML output would need to look something like this (nested lists sorted by item):
 Item1
   Item10
@@ -67,3 +25,21 @@ Show alternative solutions, and list the advantages and disadvantages of using t
 It is not necessary to include a working database connection, just show where you would place it and populate an array in such a way that it would mimic the results from a database query.
 If you are familiar with Big O notation, show your analysis.
 */
+
+$nav = [
+        [1, 'Item1', 0,],
+        [2 ,'Item2', 0 ],
+        [3, 'Item3', 0],
+        [4, 'Item10', 1],
+        [5, 'Item11', 1],
+        [6, 'Item12', 1],
+        [7, 'Item100', 4],
+        [8, 'Item101', 4],
+        [9, 'Item102', 4],
+        [10, 'Item30', 3],
+        [11, 'Item31', 3],
+        [12, 'Item32', 3],
+      ];
+
+var_dump($nav);
+?>
