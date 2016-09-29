a17cbe2ae6ea6617307e6c68acb058dd11641da4
Marc Barbeau <hello@marcbarbeau.com>
2016-09-29 08:09:09 -0600

    Hook it all up

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+  require('model.php');
+  require('view.php');
+  require('controller.php');
 
+  $model = new Model();
+  $controller = new Controller($model);
+  $view = new View($controller, $model);
+
+
+  var_dump($model);
+  var_dump($controller);
+  var_dump($view);
 ?>
