26530eafa8601abc2a79eff2d3c52105ea8b1fe2
Marc Barbeau <hello@marcbarbeau.com>
2016-09-29 11:33:02 -0600

    Output object recursively

--- view.php
+++ view.php
@@ -1,15 +1,32 @@
 <?php
 class View
 {
-    private $model;
-    private $controller;
+  public $model;
+  public $controller;
 
-    public function __construct($controller,$model) {
-        $this->controller = $controller;
-        $this->model = $model;
-    }
+  public function __construct($controller,$model)
+  {
+    $this->controller = $controller;
+    $this->model = $model->structure;
+  }
 
-    public function output(){
-        return $this->model;
-    }
+  public function output($model)
+  {
+    ?>
+    <ul>
+      <?php foreach($model as $key => $item): ?>
+        <li id= <?=$item[0];?>>
+          <?=$item[1];?>
+        </li>
+
+        <?php
+        if( !empty($item[3]) )
+          {
+            $this->output($item[3]);
+          }
+        ?>
+      <?php endforeach; ?>
+    </ul>
+    <?php
+  }
 }
