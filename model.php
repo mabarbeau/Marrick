2a6c55556f2cc6ac929dc0b38b39d4151ae20c6d
Marc Barbeau <hello@marcbarbeau.com>
2016-10-07 13:11:30 -0600

    Move data into a csv file

--- model.php
+++ model.php
@@ -7,29 +7,16 @@ class Model
 
   public function __construct()
   {
-    $this->list = [
-      [1, 'Item1', 0],
-      [2 ,'Item2', 0],
-      [3, 'Item3', 0],
-      [4, 'Item10', 1],
-      [5, 'Item11', 1],
-      [6, 'Item12', 1],
-      [7, 'Item100', 4],
-      [8, 'Item101', 4],
-      [9, 'Item102', 4],
-      [10, 'Item30', 3],
-      [11, 'Item31', 3],
-      [12, 'Item32', 3],
-    ];
+    $this->list = array_map('str_getcsv', file('data.csv'));
 
-     $this->structure = $this->walk(0);
+    $this->structure = $this->walk(0);
   }
 
   protected function filter($key)
   {
     $filtered = array_filter($this->list, function($item) use ($key)
       {
-        return $item[2] === $key;
+        return $item[2] == $key;
       }
     );
 
